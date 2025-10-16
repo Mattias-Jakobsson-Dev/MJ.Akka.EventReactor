384d50d71b42e585fd3b55c899c553c60ca52623
Mattias Jakobsson <mattias@mjdev.se>
2025-10-16 08:44:27 +0200

    Added logging

--- src/MJ.Akka.EventReactor.PositionStreamSource/PositionedStreamPublisher.1_NotStarted.cs
+++ src/MJ.Akka.EventReactor.PositionStreamSource/PositionedStreamPublisher.1_NotStarted.cs
@@ -1,6 +1,7 @@
 using System.Collections.Immutable;
 using Akka;
 using Akka.Actor;
+using Akka.Event;
 using Akka.Streams;
 using Akka.Streams.Dsl;
 using MJ.Akka.EventReactor.DeadLetter;
@@ -18,6 +19,8 @@ public partial class PositionedStreamPublisher
             _currentPosition ??= await _startPositionStream.GetInitialPosition();
 
             var self = Self;
+            
+            Log.Info("Starting positioned stream event reactor {0} from position {1}", _eventReactorName, _currentPosition);
 
             _startPositionStream
                 .StartFrom(_currentPosition)
