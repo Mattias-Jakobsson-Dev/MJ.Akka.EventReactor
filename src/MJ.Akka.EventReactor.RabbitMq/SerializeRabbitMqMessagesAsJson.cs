0d8f2f306359e01e2b71d9bfe99abf0761826c97
Mattias Jakobsson <mattias@mjdev.se>
2026-02-26 13:50:22 +0100

    Updated rabbitmq version

--- src/MJ.Akka.EventReactor.RabbitMq/SerializeRabbitMqMessagesAsJson.cs
+++ src/MJ.Akka.EventReactor.RabbitMq/SerializeRabbitMqMessagesAsJson.cs
@@ -1,38 +1,59 @@
 using System.Collections.Immutable;
+using System.Text.Json;
 using Akka.IO;
 using Akka.Streams.Amqp.RabbitMq;
 using JetBrains.Annotations;
-using Newtonsoft.Json;
+using RabbitMQ.Client;
 
 namespace MJ.Akka.EventReactor.RabbitMq;
 
 [PublicAPI]
 public class SerializeRabbitMqMessagesAsJson(Func<object, string?>? findRoutingKey = null) : IRabbitMqMessageSerializer
 {
-    private readonly JsonSerializerSettings _settings = new()
+    public const string TypeMetadataKey = "message-type";
+
+    private static readonly JsonSerializerOptions Options = new()
     {
-        TypeNameHandling = TypeNameHandling.All
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
     };
-    
+
     public Task<OutgoingMessage> Serialize(object message)
     {
-        var json = JsonConvert.SerializeObject(message, _settings);
-        
+        var json = JsonSerializer.Serialize(message, message.GetType(), Options);
+
         return Task.FromResult(new OutgoingMessage(
             ByteString.FromString(json),
-            true, 
             true,
+            true,
+            new BasicProperties
+            {
+                Headers = new Dictionary<string, object?>
+                {
+                    { TypeMetadataKey, message.GetType().AssemblyQualifiedName }
+                }
+            },
             routingKey: findRoutingKey?.Invoke(message)));
     }
 
     public Task<(object data, IImmutableDictionary<string, object?> metadata)> DeSerialize(IncomingMessage message)
     {
-        var result = JsonConvert.DeserializeObject(message.Bytes.ToString(), _settings);
-        
-        if (result == null)
-            throw new SerializationException("Failed to deserialize RabbitMQ message");
+        var json = message.Bytes.ToString();
+
+        var typeName = message.Properties.Headers?.TryGetValue(TypeMetadataKey, out var raw) == true
+            ? raw?.ToString()
+            : null;
+
+        var type = typeName != null
+            ? Type.GetType(typeName) ?? throw new SerializationException($"Could not resolve type '{typeName}'")
+            : typeof(JsonElement);
+
+        var result = JsonSerializer.Deserialize(json, type, Options)
+                     ?? throw new SerializationException("Failed to deserialize RabbitMQ message");
+
+        var metadata = ImmutableDictionary<string, object?>.Empty
+            .Add(TypeMetadataKey, message.GetType().AssemblyQualifiedName);
 
         return Task.FromResult<(object data, IImmutableDictionary<string, object?> metadata)>(
-            (result, ImmutableDictionary<string, object?>.Empty));
+            (result, metadata));
     }
 }
